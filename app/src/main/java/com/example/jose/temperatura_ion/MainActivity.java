92a61875b651aa77b92b486027eca2f7095ebb65
josé adilson <joseadilsson@gmail.com>
2017-01-06 00:34:34 -0200

    imgfim

--- app/src/main/java/com/example/jose/temperatura_ion/MainActivity.java
+++ app/src/main/java/com/example/jose/temperatura_ion/MainActivity.java
@@ -28,23 +28,31 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-//    EditText edBuscaCidade;
     private ProgressDialog progressDialog;
 
     private List<Weather> weatherList = new ArrayList<>();
     private WeatherAdapter weatherAdapter;
 
-    String cidadePesq;
-
+    private TextView lbTemperaturaDia;
+    private String cidadePesq;
+    private String cityConverter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("");
+        progressDialog = new ProgressDialog(MainActivity.this);
 
         cidadePesq = getIntent().getStringExtra("NOME_CIDADE");
-        getSupportActionBar().setTitle("");
         requisicao();
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
     }
 
     @Override
@@ -59,35 +67,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pesquisaCidade() {
-
         startActivity(new Intent(this, PesquisarCidadeActivity.class));
-
-
-//        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-//        builder.setTitle("Digite a cidade");
-//        builder.setMessage("Encontre a cidade");
-//
-//        edBuscaCidade = new EditText(MainActivity.this);
-//        builder.setView(edBuscaCidade);
-//
-//        builder.setPositiveButton("Buscar", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                requisicao();
-//            }
-//        });
-//        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.dismiss();
-//            }
-//        });
-//        builder.show();
     }
 
     public void requisicao() {
-//        String cityConverter = edBuscaCidade.getText().toString();
-        String cityConverter = cidadePesq.toString();
+        try {
+            cityConverter = cidadePesq.toString();
+        } catch (Exception e2){
+            Toast.makeText(this, "Pesquise uma cidade!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         RecyclerView rv = (RecyclerView)findViewById(R.id.recycler_view);
 
         weatherAdapter = new WeatherAdapter(weatherList);
@@ -97,95 +87,106 @@ public class MainActivity extends AppCompatActivity {
         rv.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         rv.setAdapter(weatherAdapter);
 
-        final TextView lbTemperaturaDia = (TextView) findViewById(R.id.lbTemperatura);
+        lbTemperaturaDia = (TextView) findViewById(R.id.lbTemperatura);
         final TextView lbDescDia = (TextView) findViewById(R.id.lbDescDia);
 
-        final ImageView img  = (ImageView) findViewById(R.id.img);
+        //replace
+        String cidade = cityConverter;
+        String city = cidade.replace(" ", "%20");
+        //
 
-        if (cityConverter.trim().isEmpty()) {
-            Toast.makeText(MainActivity.this, "Preencha o campo Cidade", Toast.LENGTH_SHORT).show();
-        } else {
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Buscando a cidade...");
+        progressDialog.show();
 
-            //replace
-            String cidade = cityConverter;
-            String city = cidade.replace(" ", "%20");
-            //
+        final String recebe = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + city +",ak"+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+        Ion.with(MainActivity.this)
+                .load(recebe)
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+                        if (e == null) {
+                            JsonObject object   = result.get("query").getAsJsonObject();
 
-            final String recebe = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + city +",ak"+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-            Ion.with(MainActivity.this)
-                    .load(recebe)
-                    .asJsonObject()
-                    .setCallback(new FutureCallback<JsonObject>() {
-                        @Override
-                        public void onCompleted(Exception e, JsonObject result) {
-                            if (e == null) {
-                                JsonObject object   = result.get("query").getAsJsonObject();
-
-                                try{
-                                    JsonObject object1  = object.get("results").getAsJsonObject();
-                                    JsonObject object2  = object1.get("channel").getAsJsonObject();
-                                    JsonObject object3  = object2.get("location").getAsJsonObject();
-                                    JsonObject object9  = object2.get("item").getAsJsonObject();
-                                    JsonObject object10 = object9.get("condition").getAsJsonObject();
+                            try{
+                                JsonObject object1  = object.get("results").getAsJsonObject();
+                                JsonObject object2  = object1.get("channel").getAsJsonObject();
+                                JsonObject object3  = object2.get("location").getAsJsonObject();
+                                JsonObject object9  = object2.get("item").getAsJsonObject();
+                                JsonObject object10 = object9.get("condition").getAsJsonObject();
 
 
-                                    //Local
-                                    String[] local = {object3.get("city").getAsString().toString(), " -", object3.get("region").getAsString().toString(), ", ", object3.get("country").getAsString().toString()};
-                                    String resultadoLocal = "";
-                                    for (int a = 0; a < local.length; a++) {
-                                        resultadoLocal += local[a];
-                                        getSupportActionBar().setTitle("" + resultadoLocal);
-                                    }
-                                    //
-
-                                    //dia
-                                    JsonArray jsonArray = object9.get("forecast").getAsJsonArray();
-                                    for (int i = 0; i < jsonArray.size(); i++) {
-                                        JsonArray forecast = jsonArray.getAsJsonArray();
-                                        JsonObject object11 = forecast.get(0).getAsJsonObject();
-
-                                        //Conversão
-                                        DecimalFormat formato = new DecimalFormat("##");
-                                        Double converter  = Double.valueOf(object10.get("temp").getAsString());
-                                        Double calcular  = (converter - 32) / 1.8;
-
-                                        //Temperatura, descrição do tempo do Dia
-                                        lbTemperaturaDia.setText(formato.format(calcular) + "ºc");
-                                        lbDescDia.setText(object10.get("text").getAsString());
-
+                                //Local
+                                String[] local = {object3.get("city").getAsString().toString(), " -", object3.get("region").getAsString().toString(), ", ", object3.get("country").getAsString().toString()};
+                                String resultadoLocal = "";
+                                for (int a = 0; a < local.length; a++) {
+                                    resultadoLocal += local[a];
+                                    getSupportActionBar().setTitle("" + resultadoLocal);
                                 }
-                                    //Semana Toda//
-                                    JsonArray arrayA = object9.get("forecast").getAsJsonArray();
-                                    for (int z = 0; z < arrayA.size(); z++) {
-                                        JsonArray array = arrayA.getAsJsonArray();
-                                        JsonObject objectSemana = array.get(z).getAsJsonObject();
+                                //
 
-                                        DecimalFormat formato = new DecimalFormat("##");
-                                        Double converterSemanaMax  = Double.valueOf(objectSemana.get("high").getAsString());
-                                        Double converterSemanaMin  = Double.valueOf(objectSemana.get("low").getAsString());
-                                        Double calcularMax  = (converterSemanaMax - 32) / 1.8;
-                                        Double calcularMin  = (converterSemanaMin - 32) / 1.8;
+                                //dia
+                                JsonArray jsonArray = object9.get("forecast").getAsJsonArray();
+                                for (int i = 0; i < jsonArray.size(); i++) {
+                                    JsonArray forecast = jsonArray.getAsJsonArray();
+                                    JsonObject object11 = forecast.get(0).getAsJsonObject();
 
-                                        String date = objectSemana.get("date").getAsString();
-                                        String day  = objectSemana.get("day").getAsString();
-                                        String high = formato.format(calcularMax);
-                                        String low  = formato.format(calcularMin);
-                                        String text = objectSemana.get("text").getAsString();
+                                    //Conversão
+                                    DecimalFormat formato = new DecimalFormat("##");
+                                    Double converter  = Double.valueOf(object10.get("temp").getAsString());
+                                    Double calcular  = (converter - 32) / 1.8;
 
-                                        Weather weather = new Weather(date.toString(), day.toString(), high.toString()+" ↑", low.toString()+" ↓", text.toString());
-                                        weatherList.add(weather);
-                                        weatherAdapter.notifyDataSetChanged();
+                                    //Temperatura, descrição do tempo do Dia
+                                    lbTemperaturaDia.setText(formato.format(calcular) + "ºc");
+                                    lbDescDia.setText(object10.get("text").getAsString());
+
+//                                    //Tipo da imagem
+                                    ImageView img  = (ImageView) findViewById(R.id.img);
+                                    String pegarNome  = object10.get("text").getAsString();
+
+                                    if (pegarNome.toString().equals("Partly Cloudy") || pegarNome.toString().equals("Mostly Cloudy") || pegarNome.toString().equals("Cloudy")) {
+                                        img.setImageResource(R.drawable.nube);
+                                    } if (pegarNome.toString().equals("Thunderstorms") || pegarNome.toString().equals("Scattered Thunderstorms")) {
+                                        img.setImageResource(R.drawable.tormenta);
+                                    } if (pegarNome.toString().equals("Scattered Showers") || pegarNome.toString().equals("Showers") || pegarNome.toString().equals("Rain")) {
+                                        img.setImageResource(R.drawable.lluvia);
+                                    } if (pegarNome.toString().equals("Mostly Sunny")) {
+                                        img.setImageResource(R.drawable.nubesol);
+                                    } if (pegarNome.toString().equals("clear")) {
+                                        img.setImageResource(R.drawable.sol);
+                                    } if (pegarNome.toString().equals("Snow") || pegarNome.toString().equals("Snow Showers")) {
+                                        img.setImageResource(R.drawable.snow);
                                     }
+                                }
+                                //Semana Toda//
+                                JsonArray arrayA = object9.get("forecast").getAsJsonArray();
+                                for (int z = 0; z < arrayA.size(); z++) {
+                                    JsonArray array = arrayA.getAsJsonArray();
+                                    JsonObject objectSemana = array.get(z).getAsJsonObject();
 
-                                }catch (Exception e1){
-                                    Toast.makeText(MainActivity.this, "City not found", Toast.LENGTH_SHORT).show();
+                                    DecimalFormat formato = new DecimalFormat("##");
+                                    Double converterSemanaMax  = Double.valueOf(objectSemana.get("high").getAsString());
+                                    Double converterSemanaMin  = Double.valueOf(objectSemana.get("low").getAsString());
+                                    Double calcularMax  = (converterSemanaMax - 32) / 1.8;
+                                    Double calcularMin  = (converterSemanaMin - 32) / 1.8;
+
+                                    String date = objectSemana.get("date").getAsString();
+                                    String day  = objectSemana.get("day").getAsString();
+                                    String high = formato.format(calcularMax);
+                                    String low  = formato.format(calcularMin);
+                                    String text = objectSemana.get("text").getAsString();
+
+                                    Weather weather = new Weather(date.toString(), day.toString(), high.toString()+" ↑", low.toString()+" ↓", text.toString());
+                                    weatherList.add(weather);
+                                    weatherAdapter.notifyDataSetChanged();
                                 }
 
-
+                            }catch (Exception e1){
+                                Toast.makeText(MainActivity.this, "City not found", Toast.LENGTH_SHORT).show();
                             }
                         }
-                    });
-        }
+                    }
+                });
     }
-
 }
